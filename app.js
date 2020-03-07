209aa07f5d2ed2572a35566d09fb7f044e45f262
zhangtao25 <18158899797@163.com>
2020-03-07 20:43:48 +0800

    upd

--- app.js
+++ app.js
@@ -1,52 +1,31 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const app = express();
+const multer = require('multer');
+const UUID = require('uuid');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var uploadRouter = require('./routes/upload');
 
-var app = express();
-
-//设置允许跨域访问该服务.
-app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Methods', '*');
-  res.header('Content-Type', 'application/json;charset=utf-8');
-  next();
+// multer配置
+const storage = multer.diskStorage({
+    destination: './public',
+    filename: function(req, file, cb) {
+        let extName = UUID.v1() + '.' + file.originalname.split('.').reverse()[0];
+        cb(null, extName)
+    }
+});
+const upload = multer({
+    storage: storage
 });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// 静态托管
+app.use(express.static('public'));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/upload', uploadRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// 处理upload请求
+app.post('/upload', upload.single('file'), function (req, res, next) {
+    let file = req.file;
+    res.send(file)
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen(3001,function(){
+    console.log("在运行了哦，QwQ！")
 });
-
-module.exports = app;
